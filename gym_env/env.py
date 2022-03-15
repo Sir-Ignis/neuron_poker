a44052911dc34e8d5c2f38e671bd993c69732286
Sir-Ignis <masterdaniel.dutra@googlemail.com>
2022-03-15 12:02:25 +0000

    Tested negative reward for folding

--- gym_env/env.py
+++ gym_env/env.py
@@ -331,13 +331,13 @@ class HoldemTable(Env):
     def _calculate_reward(self, action):
         """Reward function for agent"""
         reward = 0
+        pot = sum(np.minimum(self.player_max_win[1], self.player_max_win))
         if not(action == Action.FOLD):
             # reward = expected value weighted against number of table cards
             # expected value = win_amount+loss_amount
             # contribution = amount keras-rl contributed to the pot
-            contribution = self._contribution(action)
-            pot = sum(np.minimum(self.player_max_win[1], self.player_max_win))
             expected_value = 0
+            contribution = self._contribution(action)
             win_percentage = self.player_data.equity_to_river_alive
             if(action == Action.CALL or action == Action.CHECK):
                 # win_amount = win_percentage*pot
@@ -348,7 +348,9 @@ class HoldemTable(Env):
                 # loss_amount = (1-win_percentage)*contribution
                 expected_value = win_percentage*pot + ((1-win_percentage)*-contribution)
             reward = expected_value 
-        # folding has an expected value of 0
+        # folding forfeits the pot
+        else:
+            reward = -pot 
         return reward
 
 
