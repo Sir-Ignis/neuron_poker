9f162b590bc0a6b4bc0d93f5747b9ccb99822444
Sir-Ignis <masterdaniel.dutra@googlemail.com>
2022-04-26 22:56:37 +0100

    Improved GUI

--- GUI/gui/app.py
+++ GUI/gui/app.py
@@ -28,6 +28,9 @@ LOG_DEALER_POS_STRING = 'Dealer is at position'
 LOG_GUI_INFO_STRING = 'GUI INFO: '
 LOG_NEW_HAND_STRING = 'INFO - Starting new hand.'
 LOG_OPP_CARDS_STRING = 'INFO - Opponent cards: '
+LOG_WON_STRING0 = 'INFO - Player 0 won:' #you won
+LOG_WON_STRING1 = 'INFO - Player 1 won: ' #bot won
+LOG_TOTAL_WIN_STRING = 'INFO - Total winnings = '
 
 PLAY_STEP_LENGTH = 1000 # used in process start command to determine selfplay step length
 MODEL_TYPE_EQUITY = "EQUITY" # equity uses the model when the agent was trained agains the equity agent
@@ -35,6 +38,9 @@ MODEL_TYPE_RANDOM = "RANDOM" # random uses the model when the agent was trained 
 MODEL_TYPE = MODEL_TYPE_EQUITY # used in process command to determine the selfplay model used
 
 AGENT_CARDS_SHOWN = False #true when keras-rl's cards are shown on the table
+WAIT_TIME = 10000 #time to display cards and win message for
+IDLE_PERIOD = False#true when win message hasn't been cleared else false
+COMMUNITY_CARDS = 0
 
 os.chdir(PROJECT_PATH)
 
@@ -57,6 +63,8 @@ class MainWindow(QtWidgets.QMainWindow):
         # setup main_menu ui
         self.home()
         self.showMaximized()
+        self.tmp_data = QByteArray() #to hold data when idling to correctly render cards
+        self.tmp_log_action_string_shown = False
 
     def closeEvent(self, event):
         if not(self.p == None):
@@ -238,14 +246,21 @@ class MainWindow(QtWidgets.QMainWindow):
 
         log_action_string_shown = False
         if(data.contains(LOG_ACTION_STRING.encode("utf8"))):
-            # when first log_action_string is shown is when the ui is fully ready
-            self.toggle_buttons(True)
             log_action_string_shown = True
+            if not IDLE_PERIOD:
+                self.toggle_buttons(True)
         if data_.contains(LOG_GUI_INFO_STRING.encode("utf8")):
-            self.parseData(data_,log_action_string_shown)
+            if not IDLE_PERIOD:
+                self.parseData(data_,log_action_string_shown)
+            else:
+                self.tmp_data = QByteArray(data)
+                self.tmp_log_action_string_shown = log_action_string_shown
         if data_.contains(LOG_OPP_CARDS_STRING.encode("utf8")):
             self.show_agent_cards(data_)
-        if data_.contains(LOG_NEW_HAND_STRING.encode("utf8")):
+        if data_.contains(LOG_WON_STRING0.encode("utf8")) or \
+           data_.contains(LOG_WON_STRING1.encode("utf8")):
+               self.display_won_msg(data_)
+        if not IDLE_PERIOD and data_.contains(LOG_NEW_HAND_STRING.encode("utf8")):
             self.clear_table()
         if data_.contains(LOG_DEALER_POS_STRING.encode("utf8")):
             self.renderDealerButton(data_)
@@ -380,6 +395,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.opponentLastActionLabel.setText("Last Action: "+self.action_val_to_string(d['last_action']))
         self.ui.opponentChipsLabel.setText("Opponent Chips: $"+ str(d['opponent_stack']))
         table_cards = d['table_cards']
+        global COMMUNITY_CARDS
+        COMMUNITY_CARDS = len(table_cards)
         for i in range(len(table_cards)):
             if i == 0:
                 self.ui.communityCard1.setPixmap(QtGui.QPixmap(sprite_path+table_cards[0]+'.png'))
@@ -400,12 +417,15 @@ class MainWindow(QtWidgets.QMainWindow):
     def action_val_to_string(self, action_value):
         return Action(action_value).name.replace('_',' ')
 
-    def clear_table(self):
+    def clear_community_cards(self):
         self.ui.communityCard1.setPixmap(QtGui.QPixmap())
         self.ui.communityCard2.setPixmap(QtGui.QPixmap())
         self.ui.communityCard3.setPixmap(QtGui.QPixmap())
         self.ui.communityCard4.setPixmap(QtGui.QPixmap())
         self.ui.communityCard5.setPixmap(QtGui.QPixmap())
+
+    def clear_table(self):
+        self.clear_community_cards()
 
         self.ui.playerCard1.setPixmap(QtGui.QPixmap())
         self.ui.playerCard2.setPixmap(QtGui.QPixmap())
@@ -426,11 +446,55 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.oppHandCard1.setPixmap(card1)
             self.ui.oppHandCard2.setPixmap(card2)
             if not card1.isNull() and not(card2.isNull()):
-                QTimer.singleShot(2000, self.clear_agent_cards)
+                self.idle_period()
+                QTimer.singleShot(WAIT_TIME, self.clear_agent_cards)
         
     def clear_agent_cards(self):
         self.ui.oppHandCard1.setPixmap(QtGui.QPixmap())
         self.ui.oppHandCard2.setPixmap(QtGui.QPixmap())
+    
+    def idle_period(self):
+        #disable the buttons until win message is cleared
+        global IDLE_PERIOD
+        IDLE_PERIOD = True
+        self.toggle_buttons(False)
+    
+
+    def display_won_msg(self, info):
+        info_str = str(info, "utf8")
+        if info.contains(LOG_WON_STRING1.encode("utf8")):
+            
+           if COMMUNITY_CARDS == 0: 
+               self.ui.winnerLabel.setText('Bot won because you folded')
+               self.idle_period()
+           else:
+               info_str = info_str.replace(LOG_WON_STRING1,'')
+               info_str = info_str.replace(LOG_TOTAL_WIN_STRING,'Bot won: $')
+               self.ui.winnerLabel.setText('Bot won with: '+info_str)
+        else:
+           if COMMUNITY_CARDS == 0: 
+               self.ui.winnerLabel.setText('You won because the bot folded')
+               self.idle_period()
+           else:
+               info_str = info_str.replace(LOG_WON_STRING0,'')
+               info_str = info_str.replace(LOG_TOTAL_WIN_STRING,'You won: $')
+               self.ui.winnerLabel.setText('You won with: '+info_str)
+    
+        QTimer.singleShot(WAIT_TIME, self.clear_won_msg)
+
+    def clear_won_msg(self):
+        self.ui.winnerLabel.clear()
+        global IDLE_PERIOD
+        global COMMUNITY_CARDS
+        IDLE_PERIOD = False
+        COMMUNITY_CARDS = 0
+        self.clear_community_cards()
+        self.toggle_buttons(True)
+        #see parseData
+        data = self.tmp_data
+        log_action_string_shown = self.tmp_log_action_string_shown
+        self.parseData(data,log_action_string_shown)
+        
         
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
